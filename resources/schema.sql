047ba3cc52339f9c2297fd48d9cc268936adf30b
Jeroen de Graaf <hello@jero.work>
2025-10-10 09:39:59 +0200

    Add database migration templates for default saga stores
    Available as
    - raw sql
    - doctrine migration
    - phinx migration

--- resources/schema.sql
+++ resources/schema.sql
@@ -14,3 +14,12 @@ CREATE TABLE `event_store_relation` (
   PRIMARY KEY (`event_id`,`domain_tag`),
   CONSTRAINT `event_store_relation_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event_store` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `saga_store` (
+  `saga_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `saga_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` json NOT NULL,
+  `created_at` timestamp(6) NOT NULL,
+  `updated_at` timestamp(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`saga_id`, `saga_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

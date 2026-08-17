0ffbcbd16e539702d6ae5e9421e78b6cfa2e4da3
Jeroen de Graaf <hello@jero.work>
2026-08-17 20:33:11 +0200

    Implement atomic optimistic lock with SELECT ... FOR UPDATE
    Guard the optimistic lock check inside the same transaction as the event
    insert using FOR UPDATE to prevent concurrent writers from both passing
    the check. Falls back gracefully on platforms that do not support FOR
    UPDATE (e.g. SQLite).

--- tests/DoctrineDbalRdbmsEventStoreRepositoryTest.php
+++ tests/DoctrineDbalRdbmsEventStoreRepositoryTest.php
@@ -6,6 +6,7 @@ namespace Gember\RdbmsEventStoreDoctrineDbal\Test;
 
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Tools\DsnParser;
+use Gember\DependencyContracts\EventStore\Rdbms\OptimisticLockException;
 use Gember\DependencyContracts\EventStore\Rdbms\RdbmsEvent;
 use Gember\RdbmsEventStoreDoctrineDbal\DoctrineDbalRdbmsEventFactory;
 use Gember\RdbmsEventStoreDoctrineDbal\DoctrineDbalRdbmsEventStoreRepository;
@@ -37,40 +38,45 @@ final class DoctrineDbalRdbmsEventStoreRepositoryTest extends TestCase
             new DoctrineDbalRdbmsEventFactory(),
         );
 
-        $this->repository->saveEvents([
-            new RdbmsEvent(
-                '63129dc3-4a27-4242-a8bc-6f79636a6fa9',
-                ['6ae07469-0f43-4f33-979b-c783b6824ce0', '0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
-                'event_name',
-                '{"data":"some"}',
-                ['metadata' => 'some'],
-                new DateTimeImmutable('2024-12-06 12:05:04.456344'),
-            ),
-            new RdbmsEvent(
-                '707678d3-c91d-4864-9729-555b22496853',
-                ['0e76f2bd-2aae-44a4-b149-740c080e4d05'],
-                'event_name',
-                '{"data":"another_event"}',
-                ['metadata' => 'another_event'],
-                new DateTimeImmutable('2024-12-01 13:16:24.467784'),
-            ),
-            new RdbmsEvent(
-                '7ac51abe-9176-4794-8246-24b75c2ba914',
-                ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
-                'event_name_2',
-                '{"data":"another"}',
-                ['metadata' => 'another'],
-                new DateTimeImmutable('2024-12-04 13:15:26.755844'),
-            ),
-            new RdbmsEvent(
-                'd404e3c1-c782-4115-b8ec-d8cb341d87cb',
-                ['6ae07469-0f43-4f33-979b-c783b6824ce0'],
-                'event_name_3',
-                '{"data":"another"}',
-                ['metadata' => 'another3'],
-                new DateTimeImmutable('2024-12-02 13:16:24.467784'),
-            ),
-        ]);
+        $this->repository->saveEvents(
+            ['6ae07469-0f43-4f33-979b-c783b6824ce0', '0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+            ['event_name', 'event_name_2', 'event_name_3'],
+            null,
+            [
+                new RdbmsEvent(
+                    '63129dc3-4a27-4242-a8bc-6f79636a6fa9',
+                    ['6ae07469-0f43-4f33-979b-c783b6824ce0', '0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+                    'event_name',
+                    '{"data":"some"}',
+                    ['metadata' => 'some'],
+                    new DateTimeImmutable('2024-12-06 12:05:04.456344'),
+                ),
+                new RdbmsEvent(
+                    '707678d3-c91d-4864-9729-555b22496853',
+                    ['0e76f2bd-2aae-44a4-b149-740c080e4d05'],
+                    'event_name',
+                    '{"data":"another_event"}',
+                    ['metadata' => 'another_event'],
+                    new DateTimeImmutable('2024-12-01 13:16:24.467784'),
+                ),
+                new RdbmsEvent(
+                    '7ac51abe-9176-4794-8246-24b75c2ba914',
+                    ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+                    'event_name_2',
+                    '{"data":"another"}',
+                    ['metadata' => 'another'],
+                    new DateTimeImmutable('2024-12-04 13:15:26.755844'),
+                ),
+                new RdbmsEvent(
+                    'd404e3c1-c782-4115-b8ec-d8cb341d87cb',
+                    ['6ae07469-0f43-4f33-979b-c783b6824ce0'],
+                    'event_name_3',
+                    '{"data":"another"}',
+                    ['metadata' => 'another3'],
+                    new DateTimeImmutable('2024-12-02 13:16:24.467784'),
+                ),
+            ],
+        );
     }
 
     #[Test]
@@ -113,36 +119,107 @@ final class DoctrineDbalRdbmsEventStoreRepositoryTest extends TestCase
     }
 
     #[Test]
-    public function itShouldReturnNullWhenGetLastEventIdPersistedIsNotFound(): void
+    public function itShouldSaveEventsWhenNoExistingEvents(): void
     {
-        $lastEventIdPersisted = $this->repository->getLastEventIdPersisted(
+        $this->repository->saveEvents(
+            ['new-domain-tag'],
+            ['new_event_name'],
+            null,
             [
-                '072fd355-bd4e-423b-a7ba-fb1a77e32d7c',
-                '60a8635c-c769-4d19-8cae-a9571401848f',
-            ],
-            [
-                'event_name',
-                'event_name_2',
+                new RdbmsEvent(
+                    'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee',
+                    ['new-domain-tag'],
+                    'new_event_name',
+                    '{"data":"new"}',
+                    [],
+                    new DateTimeImmutable('2025-01-01 00:00:00.000000'),
+                ),
             ],
         );
 
-        self::assertNull($lastEventIdPersisted);
+        $events = $this->repository->getEvents(['new-domain-tag'], ['new_event_name']);
+
+        self::assertCount(1, $events);
+        self::assertSame('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee', $events[0]->eventId);
     }
 
     #[Test]
-    public function itShouldGetLastEventIdPersisted(): void
+    public function itShouldSaveEventsWhenLastEventIdMatches(): void
     {
-        $lastEventIdPersisted = $this->repository->getLastEventIdPersisted(
+        $this->repository->saveEvents(
+            ['0c1ff409-a4be-42f1-90dd-5d7b0130a426', '6ae07469-0f43-4f33-979b-c783b6824ce0'],
+            ['event_name', 'event_name_2'],
+            '63129dc3-4a27-4242-a8bc-6f79636a6fa9',
             [
-                '0c1ff409-a4be-42f1-90dd-5d7b0130a426',
-                '6ae07469-0f43-4f33-979b-c783b6824ce0',
-            ],
-            [
-                'event_name',
-                'event_name_2',
+                new RdbmsEvent(
+                    'bbbbbbbb-cccc-dddd-eeee-ffffffffffff',
+                    ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+                    'event_name',
+                    '{"data":"appended"}',
+                    [],
+                    new DateTimeImmutable('2025-01-01 00:00:00.000000'),
+                ),
             ],
         );
 
-        self::assertSame('63129dc3-4a27-4242-a8bc-6f79636a6fa9', $lastEventIdPersisted);
+        $events = $this->repository->getEvents(
+            ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+            ['event_name', 'event_name_2'],
+        );
+
+        self::assertCount(3, $events);
+    }
+
+    #[Test]
+    public function itShouldThrowOptimisticLockExceptionWhenLastEventIdDoesNotMatch(): void
+    {
+        self::expectException(OptimisticLockException::class);
+
+        $this->repository->saveEvents(
+            ['0c1ff409-a4be-42f1-90dd-5d7b0130a426', '6ae07469-0f43-4f33-979b-c783b6824ce0'],
+            ['event_name', 'event_name_2'],
+            'wrong-event-id',
+            [
+                new RdbmsEvent(
+                    'cccccccc-dddd-eeee-ffff-000000000000',
+                    ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+                    'event_name',
+                    '{"data":"should_not_be_saved"}',
+                    [],
+                    new DateTimeImmutable('2025-01-01 00:00:00.000000'),
+                ),
+            ],
+        );
+    }
+
+    #[Test]
+    public function itShouldNotSaveEventsWhenOptimisticLockFails(): void
+    {
+        try {
+            $this->repository->saveEvents(
+                ['0c1ff409-a4be-42f1-90dd-5d7b0130a426', '6ae07469-0f43-4f33-979b-c783b6824ce0'],
+                ['event_name', 'event_name_2'],
+                'wrong-event-id',
+                [
+                    new RdbmsEvent(
+                        'dddddddd-eeee-ffff-0000-111111111111',
+                        ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+                        'event_name',
+                        '{"data":"should_not_be_saved"}',
+                        [],
+                        new DateTimeImmutable('2025-01-01 00:00:00.000000'),
+                    ),
+                ],
+            );
+        } catch (OptimisticLockException) {
+            // Expected
+        }
+
+        $events = $this->repository->getEvents(
+            ['0c1ff409-a4be-42f1-90dd-5d7b0130a426'],
+            ['event_name', 'event_name_2'],
+        );
+
+        self::assertCount(2, $events);
     }
 }
